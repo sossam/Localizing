5d57b69ad3bbc6c74a27ae33d5c4adcc88472aac
sossam <heart_b_e@naver.com>
2020-11-27 22:42:45 +0900

    Initial Commit

---
+++ Localizing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localizing
+//
+//  Created by 김소은 on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
